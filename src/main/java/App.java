5d9de0c81ba981742b67489bbe0bba80aa362bfa
Noel Bor <phaxier@gmail.com>
2019-05-21 12:17:37 +0300

    udated squad-form.vtl

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -77,6 +77,14 @@ public class App {
             model.put("template", "templates/hero.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
+
+        get ("/squads/new", (request, response)->{
+            Map <String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/squad-form.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+
     }
 
 }
