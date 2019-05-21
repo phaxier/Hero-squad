b6d95ebd275539f872f3141ebd21455a9474cd89
Noel Bor <phaxier@gmail.com>
2019-05-21 07:54:44 +0300

    created hero.vtl file.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -69,6 +69,14 @@ public class App {
             model.put("template", "templates/index.vtl");
             return new ModelAndView(model, layout);
         },new VelocityTemplateEngine());
+
+        get("/heroes/:id", (request, response)->{
+            HashMap<String, Object> model = new HashMap<String, Object>();
+            Hero hero = Hero.find(Integer.parseInt(request.params(":id")));
+            model.put("hero", hero);
+            model.put("template", "templates/hero.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
     }
 
 }
