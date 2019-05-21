f7f8f4801d1f6141656a2fef979c46a747ace73d
Noel Bor <phaxier@gmail.com>
2019-05-21 11:29:03 +0300

    added logic to the test to return squad withthte same ID

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -22,5 +22,6 @@ public class Squad {
     public static List<Squad> all() {return instances;}
     public static void clear(){instances.clear();}
     public int getId(){return mId;}
+    public static Squad find(int id){ return instances.get(id - 1);}
 
 }
