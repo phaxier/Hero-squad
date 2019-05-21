c7a150700ecda793f16e0742acd22ecb5037cdb9
Noel Bor <phaxier@gmail.com>
2019-05-21 12:45:48 +0300

    added a route to display squads

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -95,6 +95,15 @@ public class App {
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
+        get("/ squads", (request, response)->{
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("squads", Squad.all());
+            model.put("template", "templates/squads.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+
+
 
     }
 
