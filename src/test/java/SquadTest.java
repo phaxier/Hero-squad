ebcd64993088072f59cf8389d781843f85475c41
Noel Bor <phaxier@gmail.com>
2019-05-21 09:59:50 +0300

    created tests in SquadTest.java file and created Squad.java file

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -1,2 +1,11 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
 public class SquadTest {
+
+    @Test
+    public void squad_instantiatesCorrectly_true() {
+        Squad testSquad = new Squad("Home");
+        assertEquals(true, testSquad instanceof Squad);
+    }
 }
